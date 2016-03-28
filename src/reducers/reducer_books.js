0ec7b4ca80be818d774cfedbf6b20dda82eebc4a
Seema Ullal <seema.ullal@zenpayroll.com>
2016-03-27 21:00:06 -0700

    Add books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'Javascript: The Good Parts'},
+		{ title: 'Cracking the Coding Interview'},
+		{ title: 'Eloquent Ruby'},
+		{ title: 'Object Oriented Programming'}
+	]
+}
